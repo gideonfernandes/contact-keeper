7162153c78bdf9a0906b24bd3c1f0c3a5e18999c
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-07-19 15:06:02 -0300

    Add User Model, validate register an user route data, hash passwords & return jwt token

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (request, response) => response.json({ message: 'Welcome!'}));
 
 // Define Routes
