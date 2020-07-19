475706582984e4673e3575583f597d4914cb1667
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-07-19 16:20:37 -0300

    Add Contact Model & Get all users contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route         GET api/contacts
 // @description   Get all users contacts
 // @access        Private
-router.get('/', (request, response) => {
-  response.send('Get all contacts.');
+router.get('/', auth, async (request, response) => {
+  try {
+    const contacts = await Contact
+      .find({ user: request.user.id })
+      .sort({ date: -1 });
+
+    response.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    response.status(500).send('Server Error.');
+  };
 });
 
 // @route         POST api/contacts
