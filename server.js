52f21a6b798d236ffa0f8130dd90d6a0b48382cd
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-07-19 17:38:27 -0300

    Add React setup and concurrently script

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (request, response) => response.json({ message: 'Welcome!'}));
+app.get('/', (request, response) => response.json({
+  message: 'Welcome to Contact Keeper API!'
+}));
 
 // Define Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/users', require('./routes/users'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8888;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
