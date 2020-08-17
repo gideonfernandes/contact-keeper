7a87c646c3521fa55a42d1310ff90e85eb7a51c7
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-08-17 09:13:03 -0300

    Add cors permissions

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const app = express();
 // Connect Database
 connectDB();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 // Add Cors
 app.use(cors());
 
